bc6d9d3a70190037efd1cd4adf3b81adc9bc021f
Aain Ur Rehman <aain134@gmail.com>
2023-11-30 12:37:27 +0500

    Add Contact Task

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Section from './components/Section';
 import About from './components/About';
 import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <Header />
       <Section />
       <About />
-      <Projects />
+      <Projects/>
+      <Contact/>
     </div>
   );
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Contact.css'
+function Contact() {
+  return (
+    <div className='contact-container'>
+            <h3 style={{ marginTop: '50px' }} className='contact'>Get In Touch</h3>
+<div className='paragraph-contact'>
+<p>Although I'm always looking for any new opportunities, my <br/>inbox is always open. Whether you have a question or just<br/> want to say hi, I'll try my best to get back to you!
+
+</p>
+< div style={{ transitionDelay: '500ms', marginTop: '50px' }}>
+<a href="aain134@gmail.com" className="email">Say Hello</a>
+</div>
+</div>
+    </div>
+  )
+}
+
+export default Contact
